5e426a9a7e137b39f5657f212ca873b39d171ad8
Becca Rolph <rebecca.rolph@nrel.gov>
2023-08-08 10:57:10 -0600

    finished script

--- extrapolate_results_using_learning_curves.py
+++ extrapolate_results_using_learning_curves.py
@@ -33,16 +33,18 @@ with open(cost_reductions_yaml, 'r') as stream:
 
 
 ### Apply cost reductions by looping through domains and nested loop of years
-#domains = ['gulf',  'hi', 'ma', 'na', 'sa', 'wc']
-domains = ['na'] # for debugging !!!
+domains = ['gulf',  'hi', 'ma', 'na', 'sa', 'wc']
+#domains = ['na'] # for debugging !!!
 rating = '2_20MW' # Apply cost reduction to most recent available rating
-#projected_years = ['2040', '2045', '2050']
-projected_years = ['2040'] # for debugging !!!
+projected_years = ['2040', '2045', '2050']
 
 for domain in domains:
+    print(domain)
     if domain == 'gulf':
-        rating = '2_17MWlowSP_gulf_35' 
-    ifile = gpkg_filepath + rating + '_' + domain + '.gpkg'
+        rating = '2_17MWlowSP_gulf_35'
+        ifile = gpkg_filepath + rating + '.gpkg'
+    else:
+        ifile = gpkg_filepath + rating + '_' + domain + '.gpkg'
     data = gpd.read_file(ifile)
     
     
@@ -64,19 +66,22 @@ for domain in domains:
         ### Reduce capex and opex
         ## CapEx            
         # fixed 
-        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth < 60, 'capex_kw'] = data.loc[data.depth < 60, 'capex_kw'] * capex_cost_reduction_fixed
+        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth < 60, 'capex_kw'] = data.loc[data.depth < 60, 'capex_kw'] * (1 - capex_cost_reduction_fixed)
         # floating
-        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth > 60, 'capex_kw'] = data.loc[data.depth > 60, 'capex_kw'] * capex_cost_reduction_floating
+        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth > 60, 'capex_kw'] = data.loc[data.depth > 60, 'capex_kw'] * (1 - capex_cost_reduction_floating)
             
         ## Opex
         # fixed
-        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth < 60, 'opex_kw'] = data.loc[data.depth < 60, 'opex_kw'] * opex_cost_reduction_fixed
+        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth < 60, 'opex_kw'] = data.loc[data.depth < 60, 'opex_kw'] * (1 - opex_cost_reduction_fixed)
         # floating
-        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth > 60, 'opex_kw'] = data.loc[data.depth > 60, 'opex_kw'] * opex_cost_reduction_floating
+        data_cost_reduction_applied.loc[data_cost_reduction_applied.depth > 60, 'opex_kw'] = data.loc[data.depth > 60, 'opex_kw'] * (1 - opex_cost_reduction_floating)
             
             
         ### Calculate new LCOE using the newly reduced capex and opex values
-        ## !!!
+        ## !!!lcoe_fcr: (capex * fixed_charge_rate + opex) / (cf_mean * 1000 * 8760) # opex is 1GW plant cost, AEP is now 1GW
+        fixed_charge_rate = 0.0767 # div by 100. e.g. 7.67 prcnt = 0.0767 here.
+        # ($/kW)*(1e3 kW/ 1 MW) --> $/MW
+        data_cost_reduction_applied['lcoe'] = 1e3*(data_cost_reduction_applied['capex_kw'] * fixed_charge_rate + data_cost_reduction_applied['opex_kw']) / (data_cost_reduction_applied['mean_cf'] * 8760 )
             
         # Save dataframe with reduced costs to file. note that capex, opex and lcoe no longer correspond with some other outdata of reV now (e.g. export)
         data_cost_reduction_applied.to_file(gpkg_filepath_reduced_costs + domain + '_' + rating + '_' + year + '.gpkg', driver='GPKG')
